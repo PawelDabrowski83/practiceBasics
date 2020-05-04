f9cbeb131bf0623cd1cec476dbe54e7d0c9beccd
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-04 15:44:57 +0200

    getFullname exercise

--- src/pl/basics/Main2.java
+++ src/pl/basics/Main2.java
@@ -11,4 +11,23 @@ public class Main2 {
         // write your code here
         return  Character.toString(ch).matches("[aAeEiIoOuU]");
     }
+
+    public static String getFullname(String firstName, String lastName) {
+        String result = "";
+        if (firstName != null && !firstName.isEmpty()) {
+            result = firstName;
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            if ("".equals(result)) {
+                result = lastName;
+            } else {
+                result += " " + lastName;
+            }
+
+        }
+        if (firstName == null && lastName == null || "".equals(firstName) && "".equals(lastName)) {
+            result = "Unknown";
+        }
+        return result; // write your code here
+    }
 }
--- test/pl/basics/Main2Test.java
+++ test/pl/basics/Main2Test.java
@@ -2,6 +2,7 @@ package pl.basics;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -13,11 +14,20 @@ public class Main2Test {
 
         // given
 
-        // when
+        // then
         assertTrue(Main2.isVowel('a'));
         assertFalse(Main2.isVowel('Y'));
         assertTrue(Main2.isVowel('O'));
         assertFalse(Main2.isVowel('z'));
 
+    }
+
+    @Test
+    public void shouldGetFullnameWork() {
+
+        assertEquals("Unknown", Main2.getFullname(null, null));
+        assertEquals("firstName", Main2.getFullname("firstName", null));
+        assertEquals("Unknown", Main2.getFullname("", ""));
+        assertEquals("lastName", Main2.getFullname(null, "lastName"));
     }
 }
