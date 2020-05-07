8cbc175d603f32d5d20760a77a6047ad9dedad5e
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-07 18:28:39 +0200

    byte[] CharSequence exercise

---
+++ src/pl/basics/AsciiCharSequence.java
@@ -0,0 +1,49 @@
+package pl.basics;
+
+import java.util.Arrays;
+
+public class AsciiCharSequence implements CharSequence {
+
+    private final byte[] bytes;
+
+    public AsciiCharSequence(byte[] bytes) {
+        this.bytes = Arrays.copyOf(bytes, bytes.length);
+    }
+
+    @Override
+    public int length() {
+        return this.bytes.length;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return (char) this.bytes[index];
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+
+        return new AsciiCharSequence(Arrays.copyOfRange(this.bytes, start, end));
+    }
+
+    @Override
+    public String toString() {
+
+        return new String(this.bytes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AsciiCharSequence)) return false;
+
+        AsciiCharSequence that = (AsciiCharSequence) o;
+
+        return Arrays.equals(bytes, that.bytes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(bytes);
+    }
+}
---
+++ test/pl/basics/AsciiCharSequenceTest.java
@@ -0,0 +1,78 @@
+package pl.basics;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class AsciiCharSequenceTest {
+
+    CharSequence charSequence;
+    byte[] bytes;
+
+    @Before
+    public void setUp() {
+
+        bytes = new byte[]{98, 112, 110, 111, 111, 99, 123};
+        charSequence = new AsciiCharSequence(bytes);
+
+    }
+
+    @Test
+    public void shouldLengthWork() {
+
+        assertEquals(7, charSequence.length());
+        assertNotEquals(5, charSequence.length());
+    }
+
+    @Test
+    public void shouldCharAtWork() {
+
+        // given
+        char char0Expected = (char) 98;
+        char char1Expected = (char) 112;
+        char char6Expected = (char) 123;
+
+        // when
+        char char0Actual = charSequence.charAt(0);
+        char char1Actual = charSequence.charAt(1);
+        char char6Actual = charSequence.charAt(6);
+
+        // then
+        assertEquals(char0Expected, char0Actual);
+        assertEquals(char1Expected, char1Actual);
+        assertEquals(char6Expected, char6Actual);
+        assertNotEquals(char0Expected, char6Actual);
+    }
+
+    @Test
+    public void shouldSubSequenceWork() {
+
+        // given
+        byte[] subsequenceBytes = new byte[]{110, 111, 111, 99}; // subSequence [2-6)
+        CharSequence expectedCharSequence = new AsciiCharSequence(subsequenceBytes);
+
+        // when
+        CharSequence actualCharSequence = charSequence.subSequence(2, 6);
+
+        // then
+        assertEquals(expectedCharSequence, actualCharSequence);
+        assertNotEquals(expectedCharSequence, new AsciiCharSequence(new byte[]{}));
+
+    }
+
+    @Test
+    public void shouldToStringWork() {
+
+        // given
+        String expectedString = new String(bytes);
+
+        // when
+        String actualString = charSequence.toString();
+
+        // then
+        assertEquals(expectedString, actualString);
+        assertNotEquals(expectedString, "");
+    }
+}
