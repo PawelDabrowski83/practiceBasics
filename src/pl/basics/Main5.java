398c1f9d088d4d444ca21789d91a369f5f4945b2
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-08 15:31:16 +0200

    ConvertLong - solution with ternary

--- src/pl/basics/Main5.java
+++ src/pl/basics/Main5.java
@@ -7,17 +7,22 @@ public class Main5 {
     }
 
     public static int convertLong(Long val) {
-        if (val == null) {
-            return 0;
-        } else {
-            if (val > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            } else if (val < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            } else {
-                return Math.toIntExact(val);
-            }
-        }
+    //        if (val == null) {
+    //            return 0;
+    //        } else {
+    //            if (val > Integer.MAX_VALUE) {
+    //                return Integer.MAX_VALUE;
+    //            } else if (val < Integer.MIN_VALUE) {
+    //                return Integer.MIN_VALUE;
+    //            } else {
+    //                return Math.toIntExact(val);
+    //            }
+    //        }
+        return (int) (val == null ? 0 :
+                val > Integer.MAX_VALUE ? Integer.MAX_VALUE :
+                val < Integer.MIN_VALUE ? Integer.MIN_VALUE :
+                        val);
+
 
     }
 }
