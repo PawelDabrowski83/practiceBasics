9cab7a4430d295574de55e6a3c463bc8d4e1be5a
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-15 11:28:52 +0200

    next step - counting till zero or eof appear

--- src/pl/basics/Main15_readFile.java
+++ src/pl/basics/Main15_readFile.java
@@ -2,41 +2,52 @@ package pl.basics;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main15_readFile {
 
     public static void main(String[] args) {
         /*
-        Here is a file containing a sequence of integers separated by spaces.
+        Here is a file containing a sequence of integers. Each number begins a new line.
 
-Download it and write a Java program that finds the greatest number in this file.
+Download the file and write a Java program that counts the number of even numbers in this
+ file. You should stop counting either if you get 0 or the last number is reached.
          */
 
-        String pathToFile = "files/dataset_91007.txt";
+        String pathToFile = "files/dataset_91065.txt";
         File file = new File(pathToFile);
-        String input = "";
+        List<String> input = new ArrayList<>();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                input = scanner.nextLine();
+                input.add(scanner.nextLine());
             }
         } catch (IOException e) {
             System.out.println("cannot read file: " + e.getMessage());
         }
 
-        String[] numbersAsString = input.split("\\s+");
-        int max = Integer.MIN_VALUE;
-        for (String str : numbersAsString) {
+        int count = 0;
+        boolean zeroFound = false;
+
+
+        for (String str : input) {
             try {
                 int number = Integer.parseInt(str);
-                if (number > max) {
-                    max = number;
+                if (number != 0 && number % 2 == 0) {
+                    count++;
+                }
+
+                if (number == 0) {
+                    zeroFound = true;
+                    System.out.println("count: " + count);
+                    return;
                 }
             } catch (NumberFormatException e) {
                 System.out.println("unable to read: " + e.getMessage());
             }
         }
 
-        System.out.println("max: " + max);
+        System.out.println("count: " + count);
     }
 }
