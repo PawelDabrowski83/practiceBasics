bd4ce89b63e86b0e17004828a51aa768bafeecac
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-17 19:16:55 +0200

    menu in progress - import and export

--- src/pl/basics/Main16_menu.java
+++ src/pl/basics/Main16_menu.java
@@ -1,10 +1,13 @@
 package pl.basics;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main16_menu {
 
@@ -19,6 +22,12 @@ public class Main16_menu {
     private static final String REMOVE_CARD = "The card:";
     private static final String CARD_REMOVED = "The card has been removed.";
     private static final String CARD_NOT_EXISTING = "Can't remove \"%s\": there is no such card.";
+    private static final String IMPORT_CARD = "File name:";
+    private static final String IMPORT_FAIL = "File not found.";
+    private static final String IMPORT_SUCCESS = "%d cards have been loaded.";
+    private static final Pattern CARD_DEFINITION_PATTERN = Pattern.compile("^\"(.+)\":\"(.+)\"$");
+    private static final String EXPORT_CARD = "File name:";
+    private static final String EXPORT_SUCCESS = "%d cards have been saved.";
 
     public static void main(String[] args) {
 
@@ -40,10 +49,10 @@ public class Main16_menu {
                         removeCard(scanner, cards);
                         break;
                     case "import":
-
+                        importCards(scanner, cards);
                         break;
                     case "export":
-
+                        exportCards(scanner, cards);
                         break;
                     case "ask":
 
@@ -103,5 +112,48 @@ public class Main16_menu {
             }
             System.out.println(CARD_REMOVED);
         }
+    }
+
+    public static void importCards(Scanner scanner, Map<String, String> cards) {
+
+        System.out.println(IMPORT_CARD);
+        String fileName = scanner.nextLine().trim();
+
+        File file = new File(fileName);
+        int count = 0;
+
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNextLine()) {
+                String newLine = fileScanner.nextLine();
+                Matcher matcher = CARD_DEFINITION_PATTERN.matcher(newLine);
+                if (newLine.matches(CARD_DEFINITION_PATTERN.pattern())) {
+                    cards.put(matcher.group(0), matcher.group(1));
+                    count++;
+                }
+            }
+            System.out.printf(IMPORT_SUCCESS, count);
+        } catch (IOException e) {
+            System.out.println(IMPORT_FAIL);
+        }
+    }
+
+    public static void exportCards(Scanner scanner, Map<String, String> cards) {
+
+        System.out.println(EXPORT_CARD);
+        String fileName = scanner.nextLine().trim();
+
+        File file = new File(fileName);
+        int count = 0;
+
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            for (Map.Entry<String, String> record : cards.entrySet()) {
+                fileWriter.write("\"" + record.getKey() + "\":\"" + record.getValue() + "\"" + System.lineSeparator());
+                count++;
+            }
+        } catch (IOException e) {
+            System.out.println(IMPORT_FAIL);
+            return;
+        }
+        System.out.printf(EXPORT_SUCCESS, count);
     }
 }
