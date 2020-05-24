ac3690c7157783cece798051ef595eb8e2a6a2e1
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-24 22:37:30 +0200

    syllables in progress

--- src/pl/basics/Main20_Syllables.java
+++ src/pl/basics/Main20_Syllables.java
@@ -1,8 +1,34 @@
 package pl.basics;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main20_Syllables {
 
     public static int countSyllables(String word) {
-        return 0;
+        String vowels = "[aeiouy]+";
+        Pattern vowelsPattern = Pattern.compile(vowels);
+        String tempWord = new String(word.toLowerCase());
+        if (word.matches("\\?+e$")) {
+            tempWord = tempWord.replace("e$", "");
+        }
+        Matcher matcher = vowelsPattern.matcher(tempWord);
+        System.out.println(tempWord + " " + matcher.find());
+
+        String devoweled = new String(tempWord.replaceAll(vowels, ""));
+        if (matcher.matches() || devoweled.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        System.out.println(tempWord);
+        while (vowelsPattern.matcher(tempWord).find())
+            count++;
+
+        return count;
+//        String firstLetter = String.valueOf(tempWord.charAt(0));
+//        if (firstLetter.matches(vowels)) {
+//            return word.split(vowels).length - 1;
+//        }
+//        return word.split(vowels).length - 1;
     }
 }
--- test/pl/basics/Main20_SyllablesTest.java
+++ test/pl/basics/Main20_SyllablesTest.java
@@ -7,25 +7,44 @@ import static org.junit.Assert.assertEquals;
 public class Main20_SyllablesTest {
 
     @Test
-    public void shouldCountSyllablesWork() {
+    public void shouldCountSyllablesOne() {
 
-        /*
-        Readability is the ease with which a reader can understand a written text.
-         */
-        assertEquals(5, Main20_Syllables.countSyllables("Readability"));
         assertEquals(1, Main20_Syllables.countSyllables("is"));
         assertEquals(1, Main20_Syllables.countSyllables("the"));
         assertEquals(1, Main20_Syllables.countSyllables("ease"));
         assertEquals(1, Main20_Syllables.countSyllables("with"));
         assertEquals(1, Main20_Syllables.countSyllables("which"));
         assertEquals(1, Main20_Syllables.countSyllables("a"));
-        assertEquals(2, Main20_Syllables.countSyllables("reader"));
         assertEquals(1, Main20_Syllables.countSyllables("can"));
-        assertEquals(3, Main20_Syllables.countSyllables("understand"));
-        assertEquals(2, Main20_Syllables.countSyllables("written"));
         assertEquals(1, Main20_Syllables.countSyllables("text"));
 
     }
+    @Test
+    public void shouldCountSyllablesTwoOrThree() {
+
+        assertEquals(2, Main20_Syllables.countSyllables("reader"));
+        assertEquals(3, Main20_Syllables.countSyllables("understand"));
+        assertEquals(2, Main20_Syllables.countSyllables("written"));
+        assertEquals(3, Main20_Syllables.countSyllables("nevermore"));
+        assertEquals(3, Main20_Syllables.countSyllables("employee"));
+        assertEquals(2, Main20_Syllables.countSyllables("sequence"));
+
+    }
+
+    @Test
+    public void shouldCountSyllablesMore() {
+
+        assertEquals(5, Main20_Syllables.countSyllables("Readability"));
+        assertEquals(4, Main20_Syllables.countSyllables("publication"));
+        assertEquals(8, Main20_Syllables.countSyllables("internationalization"));
+        assertEquals(5, Main20_Syllables.countSyllables("unavailable"));
+
+
+    }
+
+
+
+
 
     @Test (expected = NullPointerException.class)
     public void shouldThrowNPEGivenNull() {
