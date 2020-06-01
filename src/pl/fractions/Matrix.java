ff83d6714e50164defd7a804c47331255520e318
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-01 11:57:49 +0200

    MatrixSolver - order rows by column leading entry working

--- src/pl/fractions/Matrix.java
+++ src/pl/fractions/Matrix.java
@@ -43,21 +43,18 @@ public class Matrix {
         for (int i = 0; i < this.rows.size(); i++) {
             rowsByLeadingEntry.put(i, this.rows.get(i).findLeadingEntryColumn());
         }
-        System.out.println(">>> " + rowsByLeadingEntry);
         Map<Integer, Row> temp = new HashMap<>();
         while (temp.size() < this.rows.size()) {
             int min = rowsByLeadingEntry.values().stream()
                     .min(Comparator.naturalOrder())
                     .orElse(0);
-            temp.putAll(this.rows.entrySet().stream()
+            this.rows.entrySet().stream()
                     .filter(e -> e.getValue().findLeadingEntryColumn() == min)
-                    .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue)));
+                    .forEach(n -> temp.put(temp.size(), n.getValue()));
             rowsByLeadingEntry = rowsByLeadingEntry.entrySet().stream()
                     .filter(e -> !e.getValue().equals(min))
                     .collect(Collectors.toMap(
                             Map.Entry::getKey, Map.Entry::getValue));
-            System.out.println(min + " * " + temp);
         }
         return new Matrix(temp);
 
--- test/pl/fractions/MatrixTest.java
+++ test/pl/fractions/MatrixTest.java
@@ -46,4 +46,66 @@ public class MatrixTest {
         // then
         assertEquals(matrixExpected, matrixActual);
     }
+
+    @Test
+    public void shouldOrderByLeadingEntryWork2() {
+        // given
+        Matrix matrix = new Matrix();
+        matrix.rows = new HashMap<>();
+        Row row1 = new Row(new Fraction[]{
+                new Fraction(0, 1), new Fraction(0, 1), new Fraction(5, 1), new Fraction(9, 1)});
+        Row row2 = new Row(new Fraction[]{
+                new Fraction(0, 1), new Fraction(-1, 1), new Fraction(3, 1), new Fraction(13, 1)});
+        Row row3 = new Row(new Fraction[]{
+                new Fraction(3, 1), new Fraction(2, 7), new Fraction(-5, 1), new Fraction(13, 1)});
+        Row row4 = new Row(new Fraction[]{
+                new Fraction(1, 1), new Fraction(0, 7), new Fraction(3, -12), new Fraction(1, 2)});
+        matrix.rows.put(0, row1);
+        matrix.rows.put(1, row2);
+        matrix.rows.put(2, row3);
+        matrix.rows.put(3, row4);
+        Matrix matrixExpected = new Matrix();
+        matrixExpected.rows = new HashMap<>();
+        matrixExpected.rows.put(3, row1);
+        matrixExpected.rows.put(2, row2);
+        matrixExpected.rows.put(0, row3);
+        matrixExpected.rows.put(1, row4);
+
+        // when
+        Matrix matrixActual = matrix.orderByLeadingEntry();
+
+        // then
+        assertEquals(matrixExpected, matrixActual);
+    }
+
+    @Test
+    public void shouldOrderByLeadingEntryWork3() {
+        // given
+        Matrix matrix = new Matrix();
+        matrix.rows = new HashMap<>();
+        Row row1 = new Row(new Fraction[]{
+                new Fraction(0, 1), new Fraction(0, 1), new Fraction(5, 1), new Fraction(9, 1)});
+        Row row2 = new Row(new Fraction[]{
+                new Fraction(8, 1), new Fraction(-1, 1), new Fraction(3, 1), new Fraction(13, 1)});
+        Row row3 = new Row(new Fraction[]{
+                new Fraction(1, 1), new Fraction(2, 7), new Fraction(-5, 1), new Fraction(13, 1)});
+        Row row4 = new Row(new Fraction[]{
+                new Fraction(-3, 1), new Fraction(0, 7), new Fraction(3, -12), new Fraction(1, 2)});
+        matrix.rows.put(0, row1);
+        matrix.rows.put(1, row2);
+        matrix.rows.put(2, row3);
+        matrix.rows.put(3, row4);
+        Matrix matrixExpected = new Matrix();
+        matrixExpected.rows = new HashMap<>();
+        matrixExpected.rows.put(3, row1);
+        matrixExpected.rows.put(0, row2);
+        matrixExpected.rows.put(1, row3);
+        matrixExpected.rows.put(2, row4);
+
+        // when
+        Matrix matrixActual = matrix.orderByLeadingEntry();
+
+        // then
+        assertEquals(matrixExpected, matrixActual);
+    }
 }
