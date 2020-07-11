98b93515eddccd289ed8768641fc3a163c2079a9
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-07-11 15:43:58 +0200

    added javadoc

--- src/pl/basics3/RegexFinder.java
+++ src/pl/basics3/RegexFinder.java
@@ -4,6 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexFinder {
+    /**
+     * Check if String part is found in String sentence but not in the beginning or the end of word.
+     * @param part - word to find
+     * @param sentence - given sentence
+     * @return if true, String part is found in sentence but not in the beginning or the end of the word.
+     */
     public static boolean isRegexInMiddleWord(String part, String sentence) {
         if (part == null || sentence == null) {
             return false;
