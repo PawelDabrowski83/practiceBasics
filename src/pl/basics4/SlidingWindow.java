1a57c1e4c806b626da93f72f4b4b427f71b8e74d
fluor <pawel.d.dabrowski@gmail.com>
2020-08-10 13:34:00 +0200

    SlidingWindow in progress

--- src/pl/basics4/SlidingWindow.java
+++ src/pl/basics4/SlidingWindow.java
@@ -18,15 +18,53 @@ public class SlidingWindow {
     }
 
     public char[] findLongestUniqueStreak() {
+        if (input.length == 0) {
+            return new char[0];
+        }
+        char[] buffer = {input[0]};
+        char[] longestBuffer = buffer;
+        int startingPosition = 0;
+        int currentPosition = 0;
+        while (input.length - 1 > currentPosition) {
+            currentPosition++;
+            if (isCharPresent(input[currentPosition], buffer)) {
+                if (longestBuffer.length < buffer.length) {
+                    longestBuffer = buffer;
+                }
+                buffer = new char[]{input[currentPosition]};
+                startingPosition = currentPosition;
+            } else {
+                buffer = slideRight(startingPosition, currentPosition);
+            }
+        }
+        if (buffer.length > longestBuffer.length) {
+            longestBuffer = buffer;
+        }
 
-        return new char[0];
+        return longestBuffer;
     }
 
     protected char[] dropLeft() {
         return new char[0];
     }
 
-    protected char[] slideRight() {
-        return new char[0];
+    protected char[] slideRight(int startingPosition, int currentPosition) {
+        if (input == null || input.length == 0) {
+            return new char[0];
+        }
+        if (startingPosition > input.length) {
+            startingPosition = 0;
+        }
+        if (currentPosition > input.length) {
+            currentPosition = input.length;
+        }
+        return Arrays.copyOfRange(input, startingPosition, currentPosition);
+    }
+
+    protected static boolean isCharPresent(char given, char[] source) {
+        if (source == null || source.length == 0) {
+            return true;
+        }
+        return !new String(source).contains(String.valueOf(given));
     }
 }
--- test/pl/basics4/SlidingWindowTest.java
+++ test/pl/basics4/SlidingWindowTest.java
@@ -1,90 +1,166 @@
 package pl.basics4;
 
-import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import pl.basics4.SlidingWindow;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 public class SlidingWindowTest {
 
     @DisplayName("Should findLongestUniqueStreak() work")
     @ParameterizedTest(name = "{index} => expected={0}, given={1}")
     @MethodSource("findLongestUniqueStreakArgumentsProvider")
-    void findLongestUniqueStreak(char[] expected, char[] given) {
-        SlidingWindow slidingWindow = new SlidingWindow(given);
-        assertArrayEquals(expected, slidingWindow.findLongestUniqueStreak());
+    void findLongestUniqueStreak(char[] expected, SlidingWindow given) {
+        assertArrayEquals(expected, given.findLongestUniqueStreak());
     }
     private static Stream<Arguments> findLongestUniqueStreakArgumentsProvider() {
         return Stream.of(
                 Arguments.of(
                         new char[]{'a', 'c'},
-                        new char[]{'a', 'c', 'a'}
+                        new SlidingWindow(new char[]{'a', 'c', 'a'})
                 ),
                 Arguments.of(
                         new char[]{'a'},
-                        new char[]{'a', 'a', 'a'}
+                        new SlidingWindow(new char[]{'a', 'a', 'a'})
                 ),
                 Arguments.of(
                         new char[]{'a', 'b', 'c', 'e'},
-                        new char[]{'a', 'a', 'a', 'b', 'c', 'e', 'a'}
+                        new SlidingWindow(new char[]{'a', 'a', 'a', 'b', 'c', 'e', 'a'})
                 ),
                 Arguments.of(
                         new char[0],
-                        new char[0]
+                        new SlidingWindow(new char[0])
                 ),
                 Arguments.of(
                         new char[]{';', 'a', 't'},
-                        new char[]{'0', ';', 'a', 't', 't'}
+                        new SlidingWindow(new char[]{'0', ';', 'a', 't', 't'})
                 ),
                 Arguments.of(
                         new char[]{'x', 'y', 'z'},
-                        new char[]{'x', 'y', 'z'}
+                        new SlidingWindow(new char[]{'x', 'y', 'z'})
                 ),
                 Arguments.of(
                         new char[]{'k', 'o', 't'},
-                        new char[]{'o', 't', 't', 'k', 'o', 't'}
+                        new SlidingWindow(new char[]{'o', 't', 't', 'k', 'o', 't'})
                 )
         );
     }
 
-//    @Ignore
-//    @DisplayName("Should dropLeft() work")
-//    @ParameterizedTest(name = "{index} => expected={0}, given={1}")
-//    @MethodSource("dropLeftArgumentsProvider")
-//    void dropLeft(char[] expected, char[] given) {
-//        assertArrayEquals(expected, SlidingWindow.dropLeft(given));
-//    }
-//    private static Stream<Arguments> dropLeftArgumentsProvider() {
-//        return Stream.of(
-//                Arguments.of(
-//                        new char[]{'a', 'b'},
-//                        new char[]{'c', 'a', 'b'}
-//                ),
-//                Arguments.of(
-//                        new char[]{'x', 'y', 'z'},
-//                        new char[]{'x', 'x', 'y', 'z'}
-//                ),
-//                Arguments.of(
-//                        new char[0],
-//                        new char[]{';'}
-//                ),
-//                Arguments.of(
-//                        new char[0],
-//                        new char[0]
-//                )
-//        );
-//    }
-//
-//    @Ignore
-//    @DisplayName("slideRight() add another character to end of an array")
-//    @ParameterizedTest(name = "{index} => expected={0}, given={1}")
-//    @MethodSource("slideRightArgumentsProvider")
-//    void slideRight(char[] expected, char[] given) {
-//        assertArrayEquals(expected, SlidingWindow.slideRight(given));
-//    }
+    @DisplayName("slideRight() should add new character from input array to buffer")
+    @ParameterizedTest(name = "{index} => expected={0}, given={1}, startingPosition={2}, currentPosition={3}")
+    @MethodSource("slideRightArgumentsProvider")
+    void slideRight(char[] expected, SlidingWindow slidingWindow, int startingPosition, int currentPosition) {
+        assertArrayEquals(expected, slidingWindow.slideRight(startingPosition, currentPosition));
+    }
+    private static Stream<Arguments> slideRightArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new char[]{'a', 'b', 'c'},
+                        new SlidingWindow(new char[]{'a', 'b', 'c', 'd'}),
+                        0,
+                        3
+                ),
+                Arguments.of(
+                        new char[]{'a', 'b', 'c', 'd'},
+                        new SlidingWindow(new char[]{'a', 'b', 'c', 'd'}),
+                        0,
+                        4
+                ),
+                Arguments.of(
+                        new char[]{'x', 'y'},
+                        new SlidingWindow(new char[]{'a', 'x', 'y'}),
+                        1,
+                        3
+                ),
+                Arguments.of(
+                        new char[]{';'},
+                        new SlidingWindow(new char[]{';', ';'}),
+                        0,
+                        1
+                ),
+                Arguments.of(
+                        new char[]{'x', 'y', 'z'},
+                        new SlidingWindow(new char[]{'a', 'x', 'y', 'z', 'z'}),
+                        1,
+                        4
+                ),
+                Arguments.of(
+                        new char[0],
+                        new SlidingWindow(new char[0]),
+                        1,
+                        4
+                ),
+                Arguments.of(
+                        new char[0],
+                        new SlidingWindow(new char[0]),
+                        -1,
+                        3
+                ),
+                Arguments.of(
+                        new char[]{'x', 'y', 'z', 'z'},
+                        new SlidingWindow(new char[]{'a', 'x', 'y', 'z', 'z'}),
+                        1,
+                        13
+                )
+        );
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void shouldSlideRightGivenNullThrowsNPE() {
+        SlidingWindow slidingWindow = null;
+        slidingWindow.slideRight(0,1);
+    }
+
+    @DisplayName("isCharPresent checks if char array contains a char")
+    @ParameterizedTest(name = "{index} => expected={0}, char={1}, array={2}")
+    @MethodSource("isCharPresentArgumentsProvider")
+    void isCharPresent(boolean expected, char given, char[] source) {
+        assertEquals(expected, SlidingWindow.isCharPresent(given, source));
+    }
+    private static Stream<Arguments> isCharPresentArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        false,
+                        'a',
+                        new char[]{'a', 'b'}
+                ),
+                Arguments.of(
+                        false,
+                        'z',
+                        new char[]{'z'}
+                ),
+                Arguments.of(
+                        false,
+                        't',
+                        new char[]{'a', 'b', 'c', 't'}
+                ),
+                Arguments.of(
+                        true,
+                        'a',
+                        new char[0]
+                ),
+                Arguments.of(
+                        true,
+                        ' ',
+                        new char[]{'a', 'z', 'x'}
+                )
+        );
+    }
+
+    @Test
+    public void shouldFindLongestUniqueStreakWork() {
+        SlidingWindow slidingWindow = new SlidingWindow(new char[]{'a'});
+        assertArrayEquals(new char[]{'a'}, slidingWindow.findLongestUniqueStreak());
+    }
+
+    @Test
+    public void shouldIsCharPresentGivenNullReturnFalse() {
+        SlidingWindow slidingWindow = new SlidingWindow(null);
+        assertTrue(SlidingWindow.isCharPresent('z', null));
+    }
+
 }
