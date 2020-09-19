9d7eec82f496c9f13ed5030852c6b68aca515d8a
fluor <pawel.d.dabrowski@gmail.com>
2020-09-19 10:47:47 +0200

    introduced inner class Keypad.Key to represent single key object with press(char) method to count presses

--- src/pl/basics5/Keypad.java
+++ src/pl/basics5/Keypad.java
@@ -6,12 +6,54 @@ package pl.basics5;
  * you should allow your module to accept input in either for convenience).
  */
 public class Keypad {
+    public static final int KEYPAD_SIZE = 12;
+    public static Key[] keys = new Key[KEYPAD_SIZE];
+
+    public static void assignCharset(){
+        keys[0] = new Key(new char[]{'1'});
+        keys[1] = new Key(new char[]{'A', 'B', 'C', '2'});
+        keys[2] = new Key(new char[]{'D', 'E', 'F', '3'});
+        keys[3] = new Key(new char[]{'G', 'H', 'I', '4'});
+        keys[4] = new Key(new char[]{'J', 'K', 'L', '5'});
+        keys[5] = new Key(new char[]{'M', 'N', 'O', '6'});
+        keys[6] = new Key(new char[]{'P', 'Q', 'R', 'S', '7'});
+        keys[7] = new Key(new char[]{'T', 'U', 'V', '8'});
+        keys[8] = new Key(new char[]{'W', 'X', 'Y', 'Z', '9'});
+        keys[9] = new Key(new char[]{'*'});
+        keys[10] =new Key(new char[]{' ', '0'});
+        keys[11] =new Key(new char[]{'#'});
+    }
+
     public static int countPresses(String given) {
+        assignCharset();
         int sum = 0;
         if (given == null || given.isEmpty()) {
             return sum;
         }
 
         return sum;
+    }
+
+    static class Key{
+        public char[] chars;
+
+        public Key(char[] chars) {
+            this.chars = chars;
+        }
+
+        public char[] getChars() {
+            return chars;
+        }
+
+        public int press(char desired){
+            int index = 1;
+            for (char c : chars){
+                if (c == Character.toUpperCase(desired)){
+                    return index;
+                }
+                index++;
+            }
+            return 0;
+        }
     }
 }
--- test/pl/basics5/KeypadTest.java
+++ test/pl/basics5/KeypadTest.java
@@ -1,5 +1,6 @@
 package pl.basics5;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,6 +11,21 @@ import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 
 public class KeypadTest {
+    static Keypad.Key key1;
+    static Keypad.Key key2;
+    static Keypad.Key key7;
+    static Keypad.Key key10;
+    static Keypad.Key key11;
+
+    @BeforeAll
+    public static void setUp(){
+        key1 = new Keypad.Key(new char[]{'1'});
+        key2 = new Keypad.Key(new char[]{'A', 'B', 'C', '2'});
+        key7 = new Keypad.Key(new char[]{'P', 'Q', 'R', 'S', '7'});
+        key10 = new Keypad.Key(new char[]{'*'});
+        key11 = new Keypad.Key(new char[]{' ', '0'});
+    }
+
     @DisplayName("should countPresses work")
     @ParameterizedTest
     @MethodSource("countPressesArgumentsProvider")
@@ -21,7 +37,31 @@ public class KeypadTest {
                 Arguments.of(9, "LOL"),
                 Arguments.of(13, "HOW R U"),
                 Arguments.of(0, ""),
-                Arguments.of(9, ".LOL!")
+                Arguments.of(9, ".LOL!"),
+                Arguments.of(1, "A"),
+                Arguments.of(2, "B"),
+                Arguments.of(8, "BBBB")
+        );
+    }
+
+    @DisplayName("should inner class Key method press work")
+    @ParameterizedTest
+    @MethodSource("pressArgumentsProvider")
+    void press(int expected, char desired, Keypad.Key key){
+        assertEquals(expected, key.press(desired));
+    }
+    private static Stream<Arguments> pressArgumentsProvider(){
+        return Stream.of(
+                Arguments.of(0, 'B', key10),
+                Arguments.of(1, 'A', key2),
+                Arguments.of(2, 'b', key2),
+                Arguments.of(3, 'r', key7),
+                Arguments.of(4, 'S', key7),
+                Arguments.of(5, '7', key7),
+                Arguments.of(1, '1', key1),
+                Arguments.of(1, ' ', key11),
+                Arguments.of(0, ' ', key10),
+                Arguments.of(1, '*', key10)
         );
     }
 }
