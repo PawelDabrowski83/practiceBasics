83789c926a83e7aa911488fde307bc5dd0a9a9e1
fluor <pawel.d.dabrowski@gmail.com>
2020-09-22 09:44:31 +0200

    FactorialString - class to calculate factorial of big numbers without using BigInteger but string represented numbers

---
+++ src/pl/basics5/FactorialString.java
@@ -0,0 +1,8 @@
+package pl.basics5;
+
+/**
+ * Exercise to calculate factorial without usage of BigInteger for big numbers - calculations
+ * should be done on numbers represented as strings
+ */
+public class FactorialString {
+}
