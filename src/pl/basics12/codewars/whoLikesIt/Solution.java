9a9418ea79412dae5a3ff60b491e5a0750ea9f9b
Paweł Dąbrowski <fluorix@gmail.com>
2021-05-30 11:33:01 +0200

    Having some struggle with varargs argument in test.

--- src/pl/basics12/codewars/whoLikesIt/Solution.java
+++ src/pl/basics12/codewars/whoLikesIt/Solution.java
@@ -1,11 +1,26 @@
 package pl.basics12.codewars.whoLikesIt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
     Task described here: https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/java
  */
+
 public class Solution {
+    public static final String LIKES_ZERO = "no one likes this";
+    public static final String LIKES_ONE = "%s likes this";
+    public static final String LIKES_TWO = "%s and %s like this";
+    public static final String LIKES_THREE = "%s, %s and %s like this";
+    public static final String LIKES_FOUR_AND_MORE = "%s, %s and %d more like this";
+
     public static String whoLikesIt(String... names) {
-        //Do your magic here
+        List<String> words = Arrays.asList(names);
+
+        if (words.size() == 0) {
+            return LIKES_ZERO;
+        }
         return "";
     }
 }
--- test/pl/basics12/codewars/whoLikesIt/SolutionTest.java
+++ test/pl/basics12/codewars/whoLikesIt/SolutionTest.java
@@ -1,32 +1,44 @@
 package pl.basics12.codewars.whoLikesIt;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
     @DisplayName("Should whoLikesIt() display proper message")
-    @MethodSource("whoLikesItArgumentProvider")
-    @ParameterizedTest
-    void whoLikesIt(String expected, String... input) {
+    @Test
+    public void whoLikesIt_zeroArgs() {
         // given
+        String expected = Solution.LIKES_ZERO;
 
         // when
-        String actual = Solution.whoLikesIt(input);
+        String actual = Solution.whoLikesIt();
 
         // then
         assertEquals(expected, actual);
     }
 
+    @DisplayName("Should whoLikesIt with one name display proper message")
+    @Test
+    public void whoLikesIt_oneArg(String name) {
+        // given
+        String expected = String.format(Solution.LIKES_ONE, name);
+
+        // when
+        String actual = Solution.whoLikesIt(name);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    /*
+
+
     private static Stream<Arguments> whoLikesItArgumentProvider() {
         return Stream.of(
-                Arguments.of("no one likes this", ""),
+//                Arguments.of("no one likes this", ""),
                 Arguments.of("Peter likes this", "Peter"),
                 Arguments.of("Jacob and Alex like this", "Jacob", "Alex"),
                 Arguments.of("Max, John and Mark like this", "Max", "John", "Mark"),
@@ -34,5 +46,8 @@ public class SolutionTest {
                 Arguments.of("Alex, Jacob and 4 others like this", "Alex", "Jacob", "Mark", "Max", "Tony", "Greg")
         );
     }
+
+
+     */
 
 }
