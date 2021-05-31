02a32b414005aed1294cacfe276dc9ae6e0b74be
Paweł Dąbrowski <fluorix@gmail.com>
2021-05-31 20:21:02 +0200

    Add task for simple encryption with string concatenation.

---
+++ src/pl/basics12/codewars/simpleEncryption1/Kata.java
@@ -0,0 +1,15 @@
+package pl.basics12.codewars.simpleEncryption1;
+
+public class Kata {
+
+    public static String encrypt(final String text, final int n) {
+        // Your code here
+        return null;
+    }
+
+    public static String decrypt(final String encryptedText, final int n) {
+        // Your code here
+        return null;
+    }
+
+}
