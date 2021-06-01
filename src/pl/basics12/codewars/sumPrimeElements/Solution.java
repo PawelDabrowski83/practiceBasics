14895ee641f4d7c7ccc89c3a5274d83927e70e18
Paweł Dąbrowski <fluorix@gmail.com>
2021-06-01 19:38:19 +0200

    Add solution.

--- src/pl/basics12/codewars/sumPrimeElements/Solution.java
+++ src/pl/basics12/codewars/sumPrimeElements/Solution.java
@@ -3,7 +3,30 @@ package pl.basics12.codewars.sumPrimeElements;
 public class Solution {
 
     public static int solve(int [] arr) {
-        return 0;
+        int sum = 0;
+        if (arr == null || arr.length == 0) {
+            return sum;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (isPrime(i)) {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        int divisor = 2;
+        while (divisor < number) {
+            if (number % divisor++ == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
