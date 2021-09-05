9e1c31008a38f868e321fee964414d6936577e88
PawelDabrowski83 <pawel.d.dabrowski@gmail.com>
2021-09-05 20:23:10 +0200

    Implement helper method isCharPresent().

--- src/pl/basics13/playWithTwoStrings/Kata.java
+++ src/pl/basics13/playWithTwoStrings/Kata.java
@@ -1,11 +1,13 @@
 package pl.basics13.playWithTwoStrings;
 
+import java.util.Locale;
+
 public class Kata {
     public static String workOnStrings(String abc, String cde) {
         return "";
     }
 
     protected static boolean isCharPresent(char c, String str) {
-        return str.contains(String.valueOf(c));
+        return str.toLowerCase(Locale.ROOT).contains(String.valueOf(c));
     }
 }
